fea41204fb5e3d520eb42cb598d16712d08ed82a
MilePervan <57873132+MilePervan@users.noreply.github.com>
2024-10-09 20:45:27 +0200

    Add files via upload

---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import "fmt"
+
+
+/*Prvi zadatak
+
+func main(){
+	var firstName string = "Mile"
+	var age int = 25
+	var height float64 = 1.84
+	var isStudent = true
+	fmt.Println(firstName,age,height,isStudent)
+}
+
+
+Drugi zadatak
+
+func main(){
+
+	const pi float64 = 3.14159
+	var radius float64 = 5
+	
+ fmt.Println(2 * pi * radius)
+	}
+
+
+
+Treci zadatak
+
+func main (){
+
+	var count int
+	var temperature float64
+	var isRaining bool
+	var message string
+	fmt.Println(count,temperature,isRaining,message)
+
+	count = 123
+	temperature = 20.5
+	isRaining = false
+	message = "abc"
+	fmt.Println(count,temperature,isRaining,message)
+}
+
+
+
+Cetvrti zadatak
+*/
+func main(){
+
+	var total int = 150
+	var discount float64 = 12.5
+	var finalPrice float64 = float64(total) - discount
+
+	fmt.Println(finalPrice)
+}
+
+
+
